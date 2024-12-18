45f1ba82098330791872b828ee00bd2886932732
Shadoukun <shadoukun@gmail.com>
2024-12-17 19:17:50 -0500

    add second thread to main function. prep for regularly updating the df instance

--- src/main.rs
+++ src/main.rs
@@ -26,19 +26,43 @@ use tokio::sync::Mutex;
 use dfinstance::{DFInstance, get_df_instance};
 use api::{AppState, get_dwarves_handler, get_gamedata_handler};
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() {
     unsafe {
         let state = AppState {
             df: Arc::new(Mutex::new(get_df_instance())),
         };
 
-        let rest = Router::new()
-            .route("/data", get(get_gamedata_handler))
-            .route("/dwarves", get(get_dwarves_handler))
-            .with_state(state.clone());
+        let server = tokio::spawn({
+            let state = state.clone();
+            async move {
+                let rest = Router::new()
+                    .route("/data", get(get_gamedata_handler))
+                    .route("/dwarves", get(get_dwarves_handler))
+                    .with_state(state.clone());
 
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-        axum::serve(listener, rest).await.unwrap();
+                let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+                axum::serve(listener, rest).await.unwrap();
+            }
+        });
+
+        let update_task = tokio::spawn(async move {
+                loop {
+                    // I think since I'm not using state after a move, I don't need to clone it
+                    // and it will update the original state correctly for the server to use. ::fingers_crossed::
+                    let mut df = state.df.lock().await;
+                    // df.update();
+                    println!("Updating...");
+                    tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+                }
+            });
+
+        tokio::select! {
+            _ = server => {},
+            _ = update_task => {},
+            _ = tokio::signal::ctrl_c() => {
+                println!("Shutting down...");
+            },
+        }
     }
 }
