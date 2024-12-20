90fddd66804b593cb829d372d0cf1c268b6a31d0
Shadoukun <shadoukun@gmail.com>
2024-12-20 12:05:23 -0500

    add working pyO3 test and python test script

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,13 @@ mod data;
 mod race;
 mod util;
 
-use std::{sync::Arc, time::Duration};
+use std::{ffi::CString, fs, path::Path, sync::Arc, time::Duration};
 use axum::{routing::get, Router};
 use tokio::sync::Mutex;
+use pyo3::prelude::*;
 
 use dfinstance::DFInstance;
 use api::{AppState, get_dwarves_handler, get_gamedata_handler};
-
 const PROCESS_NAME: &str = "Dwarf Fortress.exe";
 
 #[tokio::main]
@@ -37,6 +37,7 @@ async fn main() {
                 df: Arc::new(Mutex::new(DFInstance::new(&process))),
             }
         };
+        pyo3::prepare_freethreaded_python();
 
         let server = tokio::spawn({
             let state = state.clone();
@@ -65,9 +66,37 @@ async fn main() {
             }
         });
 
+        let handle = tokio::task::spawn_blocking(move || {
+            let py_result = Python::with_gil(|py| {
+                let script_path = Path::new("script.py");
+                if !script_path.exists() {
+                    eprintln!("Error: script.py not found in the current directory");
+                    std::process::exit(1);
+                }
+
+                let script_content = fs::read_to_string(script_path)
+                    .expect("Failed to read the Python script");
+
+                let script_content_cstr = CString::new(script_content).expect("Failed to convert script content to CString");
+                let file_name = &CString::new("script.py").unwrap();
+                let module_name = &CString::new("script").unwrap();
+
+                let module = PyModule::from_code(py,
+                    script_content_cstr.as_c_str(),
+                    file_name,
+                    module_name
+                    ).expect("Failed to load the Python script as a module");
+
+                let result = module.getattr("hello").expect("Failed to get the function");
+                let value = result.call0().expect("Failed to call the function");
+                Ok::<(), pyo3::PyErr>(())
+            });
+        });
+
         tokio::select! {
             _ = server => {},
             _ = update_task => {},
+            _ = handle => {},
             _ = tokio::signal::ctrl_c() => {
                 println!("Shutting down...");
             },
