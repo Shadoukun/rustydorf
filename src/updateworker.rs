c0098f5f5ecc1ba06e9cadde77fbe17eb0fac3d9
Shadoukun <shadoukun@gmail.com>
2024-12-22 10:58:38 -0500

    generalize worker thread; add sleep_time field

--- src/updateworker.rs
+++ src/updateworker.rs
@@ -2,47 +2,58 @@ use pyo3::prelude::*;
 use std::thread;
 use std::time::Duration;
 use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 
 #[pyclass]
-pub struct UpdateWorker {
+/// This class is exposed to Python and is used to start and stop a Rust worker thread.
+pub struct RustWorker {
     running: Arc<AtomicBool>,
+    sleep_time: Arc<AtomicU64>,
 }
 
 #[pymethods]
-impl UpdateWorker {
+impl RustWorker {
     #[new]
+    /// Create a new RustWorker instance
     pub fn new() -> Self {
-        UpdateWorker {
+        RustWorker {
             running: Arc::new(AtomicBool::new(false)),
+            sleep_time: Arc::new(AtomicU64::new(60)),
         }
     }
 
-    /// This will be called from python to start the worker thread.
+    /// Start the worker thread
     pub fn start(&mut self, _py: Python, callable: PyObject) -> PyResult<()> {
         let running = self.running.clone();
+        let sleep_time = self.sleep_time.clone();
+
+        // Set the running flag to true
         running.store(true, Ordering::SeqCst);
 
         thread::spawn(move || {
+            // loop until the running flag is set to false
             while running.load(Ordering::SeqCst) {
+                // Call the Python function
                 Python::with_gil(|py| {
                     let _ = callable.call1(py, ());
                 });
 
-                thread::sleep(Duration::from_secs(1));
+                // Sleep
+                thread::sleep(Duration::from_secs(sleep_time.load(Ordering::SeqCst)));
             }
         });
-
         Ok(())
     }
 
+    /// Stop the worker thread
     pub fn stop(&mut self) {
+        // Set the running flag to false
         self.running.store(false, Ordering::SeqCst);
     }
 }
 
 #[pymodule]
-fn update_worker(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<UpdateWorker>()?;
+fn rust_worker(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<RustWorker>()?;
     Ok(())
 }
